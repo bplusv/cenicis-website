b9020ff15a3cd22dbb48cb268495b3a45b7d50f1
lusv <lusv@live.com>
2011-09-14 16:01:40 -0600

    New Home animation =)

--- jsbin/base.js
+++ jsbin/base.js
@@ -16,3 +16,50 @@ function change_cont(cont_sel, new_cont_x, new_cont_y, new_cont_width, new_cont_
 		}, { queue: false, duration: time_span }
 	);
 }
+
+function drawBlocks2(coordinates) {
+	for (i in coordinates) {
+		var block = $('<div />').addClass('gendai_block');
+		var size = Math.floor(Math.random() * 46) + 5;
+		//size = 10;
+		block.css('width', size);
+		block.css('height', size);
+		block.css('left', coordinates[i][0] - size / 2);
+		block.css('top', coordinates[i][1] - size / 2);
+		var opacity = (Math.floor(Math.random() * 61) + 15) *.01;
+		block.fadeTo(1500, opacity);
+		block.mouseenter(function() {
+			
+		});
+		block.mouseleave(function() {
+			block.removeClass('red');
+		});
+		$('div#left_content').append(block);
+	}
+}
+
+function drawBlocks(coordinates) {
+	for (i in coordinates) {
+		var block = $('<img />').addClass('gendai_block');
+		block.attr('src', '../../pix/gendai_block.png');
+		
+		var size = Math.floor(Math.random() * 101) + 10;
+		block.css('width', size);
+		block.css('height', size);
+		block.css('left', coordinates[i][0] - size / 2);
+		block.css('top', coordinates[i][1] - size / 2);
+		var opacity = (Math.floor(Math.random() * 61) + 10) *.01;
+		var fade_time = (Math.floor(Math.random() * 3000) + 500);
+		block.fadeTo(fade_time, opacity);
+		$('div#left_content').append(block);
+		
+		block.mouseenter(function() {
+			for (i = 0; i < 1; i++) {
+				$(this).fadeTo(0, 1.0);
+			}
+		});
+		block.mouseleave(function() {
+				$(this).fadeTo(500, 0);
+		});
+	}
+}
