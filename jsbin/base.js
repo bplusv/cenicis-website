39ef4cbaedd48a32e0651c1d2b46ebf00ec14550
lusv <lusv@live.com>
2011-10-12 08:07:15 -0600

    New logo, new home menu!

--- jsbin/base.js
+++ jsbin/base.js
@@ -20,8 +20,7 @@ function change_cont(cont_sel, new_cont_x, new_cont_y, new_cont_width, new_cont_
 function drawBlocks(coordinates, container_selector) {
 	for (i in coordinates) {
 		var block = $('<img />').addClass('gendai_block');
-		block.attr('src', 'http://www.uacj.mx/IIT/CenICIS/pix/gendai_block.png');
-		
+		block.attr('src', '../../pix/gendai_block.png');
 		var size = Math.floor(Math.random() * 101) + 10;
 		block.css('width', size);
 		block.css('height', size);
@@ -29,7 +28,7 @@ function drawBlocks(coordinates, container_selector) {
 		block.css('top', $(container_selector).height() / 2);
 		block.css('opacity', 0.25);
 		
-		var opacity = (Math.floor(Math.random() * 61) + 10) *.01;
+		var opacity = (Math.floor(Math.random() * 41) + 10) *.01;
 		var fade_time = (Math.floor(Math.random() * 3001) + 1500);
 
 		$(container_selector).append(block);
@@ -42,14 +41,45 @@ function drawBlocks(coordinates, container_selector) {
 		});
 		block.animate({ left: coordinates[i][0] - size / 2, top: coordinates[i][1] - size / 2 },
 						{ queue: false, duration: 1500 }).animate(
-							{ opacity: opacity}, { duration: fade_time });
+							{ opacity: opacity }, { duration: fade_time });
 	}
 }
 
-function resetBlocks(coordinates, containter_selector) {
-	$(containter_selector + ' > img').addClass('trash');
+function resetBlocks(coordinates, container_selector) {
+	$(container_selector + ' > img').addClass('trash');
 	$('.trash').stop().fadeTo(1000, 0, function() {
 		$('.trash').remove();
 	 });
-	drawBlocks(coordinates, containter_selector);
+	drawBlocks(coordinates, container_selector);
+}
+
+function drawMenu(coordinates, container_selector, links) {
+	for (i in coordinates) {
+		var link = $(links[i]).addClass('gendai_menu');
+		link.css('left', $(container_selector).width() / 2);
+		link.css('top', $(container_selector).height() / 2);
+		link.css('opacity', 0);
+		
+		link.mouseenter(function() {
+			$(this).stop().fadeTo(0, 1.0);
+			$(this).css('background-position', '100% 0%');
+		});
+		link.mouseleave(function() {
+			$(this).fadeTo(500, 0.60);
+			$(this).css('background-position', '0% 0%');
+		});
+		$(container_selector).append(link);
+		link.animate({ left: coordinates[i][0] - link.width(), top: coordinates[i][1] - link.height() },
+						{ queue: false, duration: 1500 }).animate(
+						{ opacity: 0.60 }, { duration: 3500 });;
+		
+	}
+}
+
+function resetMenu(coordinates, container_selector, links) {
+	$(container_selector + ' > a').addClass('trash');
+	$('.trash').stop().fadeTo(1000, 0, function() {
+		$('.trash').remove();
+	 });
+	drawMenu(coordinates, container_selector, links);
 }
--- jsbin/es/inicio.js
+++ jsbin/es/inicio.js
@@ -1,13 +1,21 @@
-$(document).ready(function(){				
-	points = [[110, 30], [130, 30], [150, 30], [170, 30], [190, 30],
-				[70, 70], [90, 70], [210, 70], [230, 70],
-				[70, 110], [230, 110], [250, 110],
-				[210, 150], [230, 150],
-				[170, 190], [190, 190],
-				[170, 230], [190, 230],
-				[170, 310], [190, 310]];	
-	$('div#left_inner_content').mouseleave(function() {
-		resetBlocks(points, '#left_inner_content');
-	});
+$(document).ready(function(){		
+	points = [[150, 30], [170, 30], [190, 30], [210, 30], [230, 30],
+				[110, 70], [130, 70], [250, 70], [270, 70],
+				[90, 110], [110, 110], [270, 110], [290, 110],
+				[90, 150], [110, 150], [270, 150], [290, 150],
+				[110, 190], [130, 190], [250, 190], [270, 190],
+				[130, 230], [150, 230], [230, 230], [250, 230],
+				[150, 270], [170, 270], [210, 270], [230, 270],
+				[170, 310], [210, 310],
+				[170, 350], [190, 350], [210, 350]];		
+	menu_points = [[130, 30], [90, 75], [65, 120], [70, 165], [80, 210], [105, 255]];
+	var links = ['<a href="./contacto.html">Contacto</a>',
+				'<a href="./gente.html">Gente</a>',
+				'<a href="./mision-y-vision.html">Misi&oacute;n y Visi&oacute;n</a>',
+				'<a href="./objetivos.html">Objetivos</a>',
+				'<a href="./proyectos.html">Proyectos</a>',
+				'<a href="./servicios.html">Servicios</a>'];
+	
 	drawBlocks(points, 'div#left_inner_content');
+	drawMenu(menu_points, 'div#left_inner_content', links);
 });
