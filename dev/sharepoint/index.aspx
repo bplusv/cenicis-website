a078b3a537cd28f89d6685abb5c4e22eb58c6c56
lusv <lusv@live.com>
2012-01-18 15:59:02 -0700

    Cenicis - v1.1 (online)
    -Sharepoint dynamic iFrame wrapper

---
+++ dev/sharepoint/index.aspx
@@ -0,0 +1,25 @@
+<%@ Page masterpagefile="~masterurl/default.master" language="C#" title="UACJ Ingeniería de software - Cenicis" inherits="Microsoft.SharePoint.WebPartPages.WebPartPage, Microsoft.SharePoint, Version=12.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" meta:progid="SharePoint.WebPartPage.Document" %>
+<%@ Register tagprefix="WebPartPages" namespace="Microsoft.SharePoint.WebPartPages" assembly="Microsoft.SharePoint, Version=12.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<asp:Content id="Content1" runat="server" contentplaceholderid="PlaceHolderMain" >
+<meta name="author" content="Luis Eduardo Salazar Valles" />
+<meta name="description" content="UACJ Centro de Software" />
+<meta name="keywords" content="UACJ,software,programadores,ingeniería de software,aplicaciones móviles,UML,
+		PSP,ingeniería de requisitos,ingeniería del conocimiento,páginas web" />
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+<title>UACJ - Desarrollo de Software</title>
+<h1 style="display:none;"><a href="http://www.uacj.mx/IIT/CenICIS/Paginas/www/es/inicio.html">UACJ - Desarrollo de Software</a></h1>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="http://www.uacj.mx/IIT/CenICIS/Paginas/jsbin/jquery.autoheight.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('body').css('background', '#fff');
+		$('.shadowLeft').css('background', '#fff');
+		$('.shadowRight').css('background', '#fff');
+	});
+</script>
+
+<iframe id="dyna" class="autoHeight" style="position:relative;left:-57px;width:720px;overflow:hidden;" scrolling="no" frameborder="0" src="http://www.uacj.mx/IIT/CenICIS/Paginas/www/es/inicio.html">
+Your browser does not support inline frames or is currently configured not to display inline frames.
+</iframe>
+
+</asp:Content>
--- jsbin/base.js
+++ jsbin/base.js
@@ -1,5 +1,55 @@
-jQuery.easing['jswing'] = jQuery.easing['swing'];
+/*
+ * Cenicis - WebBase Animation v1.1
+ *
+ * TERMS OF USE - Cenicis - WebBase Animation v1.1
+ * 
+ * Open source under the BSD License. 
+ * 
+ * Copyright © 2012 Luis Eduardo Salazar Valles
+ * All rights reserved.
+ *
+ *	o         o                
+ *	|         |                
+ *	O-o  o-o  | o  o o-o o   o 
+ *	|  | |  | | |  |  \   \ /  
+ *	o-o  O-o  o o--o o-o   o   
+ *		 |                     
+ *		 o                     
+ *
+ *	Have fun. Don't forget to bookmark this website :)
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * Neither the name of the author nor the names of contributors may be used to endorse 
+ * or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
 
+/*
+ * TERMS OF USE - jQuery Easing
+ * 
+ * Open source under the BSD License. 
+ * jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
+ * Copyright © 2008 George McGinley Smith
+ */
+jQuery.easing['jswing'] = jQuery.easing['swing'];
 jQuery.extend( jQuery.easing,
 {
 	easeOutElastic: function (x, t, b, c, d) {
